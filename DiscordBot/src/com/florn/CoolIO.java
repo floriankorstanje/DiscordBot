8f0a8f246f95a3839a5db3604d611f85914fe9db
Florian <66821749+floriankorstanje@users.noreply.github.com>
2020-08-04 20:02:16 +0200

    Uploaded all the source code

---
+++ DiscordBot/src/com/florn/CoolIO.java
@@ -0,0 +1,39 @@
+package com.florn;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class CoolIO {
+    public static List<String> readSmallTextFile(String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        return java.nio.file.Files.readAllLines(path, StandardCharsets.UTF_8);
+    }
+
+    public static void writeSmallTextFile(List<String> lines, String fileName) throws IOException {
+        Path path = Paths.get(fileName);
+        java.nio.file.Files.write(path, lines, StandardCharsets.UTF_8);
+    }
+
+    public static String getPageContents(URL url) throws IOException {
+        Scanner sc = new Scanner(url.openStream());
+        StringBuffer sb = new StringBuffer();
+
+        while(sc.hasNext()) {
+            sb.append(sc.next());
+        }
+
+        String result = sb.toString();
+        System.out.println(result);
+        //Removing the HTML tags
+        result = result.replaceAll("<[^>]*>", "");
+
+        System.out.println("Contents of the web page: " + result);
+
+        return result;
+    }
+}
